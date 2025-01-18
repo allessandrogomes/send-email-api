b2935b3f3d461c5238390502ea59d0b692b16a46
Alessandro Gomes <alllessandrogomes@gmail.com>
2025-01-18 02:04:12 -0300

    :bug:fix: ajusta estrutura CORS.

--- api/send-email.js
+++ api/send-email.js
@@ -1,49 +1,44 @@
-require('dotenv').config();
-const express = require('express');
 const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
+module.exports = async (req, res) => {
+    // Configuração do CORS
+    const corsOptions = {
+        origin: 'https://valebytes.com.br', // Domínio do seu front-end
+        methods: 'GET,POST',
+        allowedHeaders: ['Content-Type'],
+    };
 
-// Configuração do CORS para permitir requisições do seu domínio específico
-app.use(cors({
-    origin: 'https://valebytes.com.br', // Permite somente esse domínio
-    methods: 'GET,POST',                // Permite apenas os métodos GET e POST
-    allowedHeaders: ['Content-Type'],   // Permite apenas o cabeçalho Content-Type
-}));
+    // Aplica a configuração do CORS à requisição
+    cors(corsOptions)(req, res, async () => {
+        if (req.method === 'POST') {
+            const { name, phone, email, message } = req.body;
 
-app.use(bodyParser.json());
+            const transporter = nodemailer.createTransport({
+                host: "smtp.hostinger.com",
+                port: 465,
+                secure: true,
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASS
+                }
+            });
 
-app.post('/send-email', async (req, res) => {
-    const { name, phone, email, message } = req.body;
+            try {
+                await transporter.sendMail({
+                    from: `${name} <contato@valebytes.com.br>`,
+                    to: "contato@valebytes.com.br",
+                    subject: `Projeto de ${name}`,
+                    text: `${message}\n\n${phone}\n\n${email}`
+                });
 
-    const transporter = nodemailer.createTransport({
-        host: "smtp.hostinger.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+                return res.status(200).json({ message: "Email enviado com sucesso!" });
+            } catch (error) {
+                console.error(error);
+                return res.status(500).json({ message: "Erro ao enviar o email." });
+            }
+        } else {
+            return res.status(405).json({ message: "Método não permitido." });
         }
     });
-
-    try {
-        await transporter.sendMail({
-            from: `${name} <contato@valebytes.com.br>`,
-            to: "contato@valebytes.com.br",
-            subject: `Projeto de ${name}`,
-            text: `${message}\n\n${phone}\n\n${email}`
-        });
-
-        res.status(200).json({ message: "Email enviado com sucesso!" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Erro ao enviar o email." });
-    }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+};
