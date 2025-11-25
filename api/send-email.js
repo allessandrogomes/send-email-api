d2a249a804726d279b5008604baf8ff7569027b9
Alessandro <alllessandrogomes@gmail.com>
2025-11-25 01:12:53 -0300

    .

--- api/send-email.js
+++ api/send-email.js
@@ -4,12 +4,9 @@ import Cors from 'cors';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const cors = Cors({
-  methods: ['POST', 'OPTIONS'],
-  origin: [
-    "http://localhost:3001",
-    "https://valebytes.com.br",
-    "https://www.valebytes.com.br"
-  ]
+  origin: ['https://valebytes.com.br', 'https://www.valebytes.com.br'],
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
 });
 
 function runMiddleware(req, res, fn) {
@@ -26,24 +23,24 @@ function runMiddleware(req, res, fn) {
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
-  if (req.method !== "POST") {
+  if (req.method === 'POST') {
+    const { name, phone, email, message } = req.body;
+
+    try {
+      await resend.emails.send({
+        from: "ValeBytes <contato@valebytes.com.br>",
+        to: "contato@valebytes.com.br",
+        reply_to: email,
+        subject: `Projeto de ${name}`,
+        text: `${message}\n\nTelefone: ${phone}\nEmail: ${email}`
+      });
+
+      return res.status(200).json({ message: "Email enviado com sucesso!" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Erro ao enviar o email." });
+    }
+  } else {
     return res.status(405).json({ message: "Método não permitido." });
-  }
-
-  const { name, phone, email, message } = req.body;
-
-  try {
-    await resend.emails.send({
-      from: "ValeBytes <contato@valebytes.com.br>",
-      to: "contato@valebytes.com.br",
-      reply_to: email,
-      subject: `Projeto de ${name}`,
-      text: `${message}\n\nTelefone: ${phone}\nEmail: ${email}`
-    });
-
-    return res.status(200).json({ message: "Enviado!" });
-  } catch (err) {
-    console.error("Erro ao enviar email:", err);
-    return res.status(500).json({ message: "Erro ao enviar." });
   }
 }
