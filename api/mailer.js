f3b464ba78394d4e637ce20954711222d2726003
Alessandro <alllessandrogomes@gmail.com>
2025-12-14 20:32:27 -0300

    .

--- api/mailer.js
+++ api/mailer.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // 587 = TLS
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
